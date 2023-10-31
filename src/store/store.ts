6899e2bb2c56fe01574407d749b83fb34a5a4531
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-10-31 14:39:26 +0300

    register reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import todoSlice from "./todoSlice";
 
 export const store = configureStore({
-    reducer: {}
-})
+  reducer: todoSlice,
+});
