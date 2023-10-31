daa43422618adb1ac4a73734cbb6179d4362e551
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-10-31 13:58:51 +0300

    create boilerplate for the application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import store from './src/store/store';
+import TodoListScreen from './src/screens/TodoListScreen';
+import AddTodoScreen from './src/screens/AddTodoScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Todo List" component={TodoListScreen} />
+            <Tab.Screen name="Add Todo" component={AddTodoScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
