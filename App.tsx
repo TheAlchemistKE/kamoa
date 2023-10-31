b6500846e8b12d4a8178f50326b370c053572f99
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-10-31 14:40:15 +0300

    refactor `App.tsx`

--- App.tsx
+++ App.tsx
@@ -1,24 +1,12 @@
-// App.tsx
-
-import React from 'react';
-import { Provider } from 'react-redux';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import store from './src/store/store';
-import TodoListScreen from './src/screens/TodoListScreen';
-import AddTodoScreen from './src/screens/AddTodoScreen';
-
-const Tab = createBottomTabNavigator();
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
+import {Todo} from "./src/screens/Todo";
 
 export default function App() {
   return (
-      <Provider store={store}>
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Todo List" component={TodoListScreen} />
-            <Tab.Screen name="Add Todo" component={AddTodoScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </Provider>
+    <Provider store={store}>
+      <Todo />
+    </Provider>
   );
 }
