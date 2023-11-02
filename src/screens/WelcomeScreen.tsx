8912078883bc30c2fab50b46be625fc1cc8cc856
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-11-02 21:08:14 +0300

    refactor welcome screen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import {Button, Dimensions, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import { Image } from "expo-image";
 export default function WelcomeScreen({ navigation }: any) {
   const handleRegisterPress = () => {
     navigation.navigate("Register");
@@ -8,10 +9,52 @@ export default function WelcomeScreen({ navigation }: any) {
     navigation.navigate("Login");
   };
   return (
-    <View>
-      <Text>Welcome Screen</Text>
-      <Button title="Create Account" onPress={handleRegisterPress} />
-      <Button title="Login" onPress={handleLoginPress} />
+    <View style={styles.container}>
+        <Text style={styles.title}>Welcome To Kamoa Todo</Text>
+      <Image source={require('../../assets/todo.png')} contentFit={'fill'} contentPosition={'center'} fadeDuration={5} style={styles.image}/>
+        <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={handleRegisterPress}
+        >
+            <Text style={styles.buttonText}>Create Account</Text>
+        </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: "black",
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 40,
+        backgroundColor: '#fff'
+    },
+    buttonContainer: {
+        marginVertical: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 10,
+        width: Dimensions.get("window").width - 20,
+        height: 44,
+        borderRadius: 5,
+        backgroundColor: "#343434",
+    },
+    buttonText: {
+        fontSize: 18,
+        color: "#ffffff",
+    },
+
+    image: {
+        width: '100%',
+        height: '80%',
+        justifyContent: 'center',
+        alignItems: "center"
+    }
+});
