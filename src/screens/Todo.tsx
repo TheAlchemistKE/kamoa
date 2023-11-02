8a69e51857e11b321d13d928ab8613b82bca65a5
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-11-02 21:07:33 +0300

    add todo list component

--- src/screens/Todo.tsx
+++ src/screens/Todo.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
+import TodoList from "../components/TodoList";
 
 export const Todo = () => {
   return (
     <View>
       <Text>Todos App</Text>
+      <TodoList />
     </View>
   );
 };
