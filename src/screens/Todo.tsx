c75b72222d9918fc04afb8c27a59cac01c06ad4c
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-11-03 14:58:29 +0300

    add styling

--- src/screens/Todo.tsx
+++ src/screens/Todo.tsx
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import {View, Text, StyleSheet, Dimensions} from "react-native";
 import TodoList from "../components/TodoList";
 
 export const Todo = () => {
   return (
-    <View>
-      <Text>Todos App</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Todos</Text>
       <TodoList />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    title: {
+        color: "black",
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 40,
+        backgroundColor: '#fff'
+    },
+});
