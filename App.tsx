b593979393a0bdcdc4727201ce1829ba3516fb5b
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-11-03 14:56:50 +0300

    add redux provider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 import AppNavigator from "./src/navigation/AppNavigator";
+import {Provider} from "react-redux";
+
+import {Todo} from "./src/screens/Todo";
+import store from "./src/store/store";
 
 export default function App() {
-  return <AppNavigator />;
+    return (
+        <Provider store={store}>
+            <AppNavigator />
+        </Provider>
+    );
 }
